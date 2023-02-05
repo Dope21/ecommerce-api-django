d212d30a3e1fadda8ed9c91c4351bc8d8342e1a6
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-02-05 18:31:24 +0700

    create folder for models and urls

--- api/models/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser
-
-# class User(AbstractBaseUser):
-#   email = models.EmailField(unique=True)
-#   password = models.CharField(max_lenght = 20)
